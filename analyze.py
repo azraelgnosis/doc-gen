9e4affc014ae596d64d1ebc6d47e34d216b061cc
azraelgnosis <azraelgnosis@gmail.com>
2020-11-03 02:54:18 -0500

    quick test

--- analyze.py
+++ analyze.py
@@ -1,3 +1,4 @@
+import re
 import os
 
 dir_path = os.path.dirname(__file__)
@@ -11,9 +12,22 @@ class Module:
     def __init__(self, name):
         self.name = name
 
-    def __repr__(self): f"{self.name}"
+    def __repr__(self): return f"{self.name}"
+
+class_pattern = "class (\w*)(\(.*\))?:$"
+function_pattern = "def (\w*)\(.*\).*:$"
 
 with open(file_path) as f:
-    module_name = f.name.split("\\")[-1].split(".")[0]
+    module_name = os.path.basename(f.name).split(".")[0]
+    file_contents = f.readlines()
 
 module = Module(module_name)
+
+for line in file_contents:
+    words = line.strip().split(" ")
+    if words[0] == "class":
+        classname = words[1].split("")
+    elif words[0] == "def":
+        pass
+
+print("done")
